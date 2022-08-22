aa2b3c3cffa05cb3d1972e84add42ba8b0672198
codeForGood300 <vardhan2011harsh@gmail.com>
2022-08-23 01:39:23 +0530

    Spring JDBC insert query

--- src/main/java/io/datajek/springdatajdbc/PlayerDao.java
+++ src/main/java/io/datajek/springdatajdbc/PlayerDao.java
@@ -20,4 +20,18 @@ public class PlayerDao {
         String sql = "SELECT * FROM PLAYER";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
     }
+
+    public Player getPlayerById(int id) {
+        String sql = "SELECT * FROM PLAYER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql,
+                new BeanPropertyRowMapper<Player>(Player.class),
+                new Object[] {id});
+    }
+
+    public int insertPlayer(Player player){
+        String sql = "INSERT INTO PLAYER (ID, Name, Nationality, Birth_date, Titles) VALUES (?, ?, ?, ?, ?)";
+        return jdbcTemplate.update(sql,
+                new Object[] {player.getId(), player.getName(), player.getNationality(),
+                        new Timestamp(player.getBirthDate().getTime()), player.getTitles()});
+    }
 }
