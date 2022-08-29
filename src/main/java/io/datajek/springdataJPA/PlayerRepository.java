3e5b09763be9adba47b152373d114a0f401187d9
codeForGood300 <vardhan2011harsh@gmail.com>
2022-08-29 16:24:34 +0530

    CRUD operation springJPA

--- src/main/java/io/datajek/springdataJPA/PlayerRepository.java
+++ src/main/java/io/datajek/springdataJPA/PlayerRepository.java
@@ -13,5 +13,16 @@ public class PlayerRepository {
     @PersistenceContext
     EntityManager entityManager;
 
+    public Player updatePlayer(Player player){
+        return entityManager.merge(player);
+    }
+
+    public Player getPlayerById(int id) {
+        return entityManager.find(Player.class, id);
+    }
+
+    public Player insertPlayer(Player player){
+        return entityManager.merge(player);
+    }
 
 }
